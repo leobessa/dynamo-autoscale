745beb960b95970b7c490ad0b2b6e131411ba15c
Sam Rose <samwho@lbak.co.uk>
2013-07-04 16:32:23 +0100

    Fixed a but that prevented scaling up and bumped version.

--- lib/dynamo-autoscale/dynamo_actioner.rb
+++ lib/dynamo-autoscale/dynamo_actioner.rb
@@ -14,7 +14,7 @@ module DynamoAutoscale
         :write_capacity_units
       end
 
-      dynamo_scale(table, aws_throughput_key => metric)
+      dynamo_scale(aws_throughput_key => metric)
     end
 
     def scale_both reads, writes
--- lib/dynamo-autoscale/version.rb
+++ lib/dynamo-autoscale/version.rb
@@ -1,3 +1,3 @@
 module DynamoAutoscale
-  VERSION = '0.2.3'
+  VERSION = '0.2.4'
 end
